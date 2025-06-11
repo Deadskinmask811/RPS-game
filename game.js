673cd14ff498a071277b28929e7ef290f5388d20
Deadskinmask811 <cstover811@gmail.com>
2025-06-11 06:13:35 -0400

    Added function to calculate computers choice

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+    let choice = Math.floor((Math.random() * 100) / 33.333);
+    switch (choice) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
+}
+
+function getHumanChoice() {
+}
+console.log(getComputerChoice());
