cb563760c6ebaf1c84577523ef3ba989c8927163
Deadskinmask811 <cstover811@gmail.com>
2025-06-11 06:49:10 -0400

    Added functionality to test equality between user and ai choices

--- game.js
+++ game.js
@@ -2,14 +2,29 @@ function getComputerChoice() {
     let choice = Math.floor((Math.random() * 100) / 33.333);
     switch (choice) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
     }
 }
 
 function getHumanChoice() {
+    let choice = prompt("Enter your choice (Rock, Paper or Scissors: ");
+    return choice.toLowerCase();
 }
-console.log(getComputerChoice());
+
+function playRound(humanChoice, computerChoice) {
+    let playerChoice = humanChoice;
+    let aiChoice = computerChoice;
+    if (playerChoice === aiChoice) {
+        console.log("Tie!");
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+playRound(getHumanChoice(), getComputerChoice());
+
