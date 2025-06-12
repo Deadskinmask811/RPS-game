9c65eb903d88e8bc0c2a12412ce2457017e46a3f
Deadskinmask811 <cstover811@gmail.com>
2025-06-12 05:48:41 -0400

    Added functionality to play a round of RPS AND to play a game of user determined rounds

--- game.js
+++ game.js
@@ -15,16 +15,60 @@ function getHumanChoice() {
     return choice.toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) {
-    let playerChoice = humanChoice;
-    let aiChoice = computerChoice;
-    if (playerChoice === aiChoice) {
-        console.log("Tie!");
+function playGame(numOfRounds) {
+    let humanScore = 0;
+    let computerScore = 0;
+    let rounds = numOfRounds;
+
+    function playRound(humanChoice, computerChoice) {
+        let playerChoice = humanChoice;
+        let aiChoice = computerChoice;
+        if (playerChoice === aiChoice) {
+            console.log("Tie!");
+            return;
+        }
+        switch (playerChoice) {
+            case "rock":
+                if (aiChoice === "paper") {
+                    console.log("You lose");
+                    return computerScore++;
+                } else {
+                    console.log("You Win!");
+                    return humanScore++;
+                }
+            case "paper":
+                if (aiChoice === "scissors") {
+                    console.log("You lose");
+                    return computerScore++;
+                } else {
+                    console.log("You Win!");
+                    return humanScore++;
+                }
+            case "scissors":
+                if (aiChoice === "rock") {
+                    console.log("You lose");
+                    return computerScore++;
+                } else {
+                    console.log("You Win!");
+                    return humanScore++;
+                }
+        }
+    }
+    //play 5 rounds
+    for (let i = 0; i < rounds; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+    //declare winner
+    if (humanScore == computerScore) {
+        console.log("TIE!");
+        return;
+    } else if (humanScore > computerScore) {
+        console.log("You win!");
+        return;
+    } else {
+        console.log("Computer wins!");
+        return;
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
-playRound(getHumanChoice(), getComputerChoice());
-
+playGame(5);
