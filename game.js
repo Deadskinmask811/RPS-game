197ad12aad4f7c4053cdbcaf896fadbee7bcfa21
Deadskinmask811 <cstover811@gmail.com>
2025-06-16 17:31:27 -0400

    added UI and stuff

--- game.js
+++ game.js
@@ -1,3 +1,54 @@
+let humanScore = 0;
+let computerScore = 0;
+const scoreToWin = 5;
+
+const scores = document.querySelector("#scores")
+const container = document.querySelector("#container");
+
+const humanScoreDisplay = document.createElement("h1");
+const computerScoreDisplay = document.createElement("h1");
+humanScoreDisplay.textContent = humanScore;
+computerScoreDisplay.textContent = computerScore;
+
+const rockButton = document.createElement("button");
+rockButton.textContent = "ROCK!";
+const paperButton = document.createElement("button");
+paperButton.textContent = "PAPER!";
+const scissorsButton = document.createElement("button");
+scissorsButton.textContent = "SCISSORS!";
+
+scores.appendChild(humanScoreDisplay);
+scores.appendChild(computerScoreDisplay);
+
+container.appendChild(rockButton);
+container.appendChild(paperButton);
+container.appendChild(scissorsButton);
+
+rockButton.addEventListener("click", playRock);
+paperButton.addEventListener("click", playPaper);
+scissorsButton.addEventListener("click", playScissors);
+
+function playRock() {
+    playRound("rock", getComputerChoice());
+}
+
+function playPaper() {
+    playRound("paper", getComputerChoice());
+}
+
+function playScissors() {
+    playRound("scissors", getComputerChoice());
+}
+
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+    humanScoreDisplay.textContent = humanScore;
+    computerScoreDisplay.textContent = computerScore;
+
+
+}
+
 function getComputerChoice() {
     let choice = Math.floor((Math.random() * 100) / 33.333);
     switch (choice) {
@@ -10,65 +61,52 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("Enter your choice (Rock, Paper or Scissors: ");
-    return choice.toLowerCase();
-}
+function playRound(string, computerChoice) {
+    let playerChoice = string;
+    let aiChoice = computerChoice;
 
-function playGame(numOfRounds) {
-    let humanScore = 0;
-    let computerScore = 0;
-    let rounds = numOfRounds;
+    switch (playerChoice) {
+        case "rock":
+            if (aiChoice === "paper") {
+                computerScore++;
+                break;
+            } else if (playerChoice === aiChoice) {
+                break;
+            } else {
+                humanScore++;
+                break;
+            }
+        case "paper":
+            if (aiChoice === "scissors") {
+                computerScore++;
+                break;
+            } else if (playerChoice === aiChoice) {
+                break;
+            } else {
+                humanScore++;
+                break;
+            }
+        case "scissors":
+            if (aiChoice === "rock") {
+                computerScore++;
+                break;
+            } else if (playerChoice === aiChoice) {
+                break;
+            } else {
+                humanScore++;
+                break;
+            }
+    }
 
-    function playRound(humanChoice, computerChoice) {
-        let playerChoice = humanChoice;
-        let aiChoice = computerChoice;
-        if (playerChoice === aiChoice) {
-            console.log("Tie!");
-            return;
-        }
-        switch (playerChoice) {
-            case "rock":
-                if (aiChoice === "paper") {
-                    console.log("You lose");
-                    return computerScore++;
-                } else {
-                    console.log("You Win!");
-                    return humanScore++;
-                }
-            case "paper":
-                if (aiChoice === "scissors") {
-                    console.log("You lose");
-                    return computerScore++;
-                } else {
-                    console.log("You Win!");
-                    return humanScore++;
-                }
-            case "scissors":
-                if (aiChoice === "rock") {
-                    console.log("You lose");
-                    return computerScore++;
-                } else {
-                    console.log("You Win!");
-                    return humanScore++;
-                }
-        }
-    }
-    //play 5 rounds
-    for (let i = 0; i < rounds; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    //declare winner
-    if (humanScore == computerScore) {
-        console.log("TIE!");
-        return;
-    } else if (humanScore > computerScore) {
+    humanScoreDisplay.textContent = humanScore;
+    computerScoreDisplay.textContent = computerScore;
+
+    if (humanScore === scoreToWin) {
         console.log("You win!");
-        return;
-    } else {
-        console.log("Computer wins!");
-        return;
+        resetGame();
+    } else if (computerScore === scoreToWin) {
+        console.log("Computer Wins!");
+        resetGame();
     }
-}
 
-playGame(5);
+}
